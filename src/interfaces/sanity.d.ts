578bf42c3cb4b9612202c38626c05322757b79b0
Tarcisio <tarcisioandrade016@gmail.com>
2023-11-10 11:55:12 -0300

    feat: sanity and project data interface

---
+++ src/interfaces/sanity.d.ts
@@ -0,0 +1,57 @@
+interface SanityBody {
+  _createdAt: string;
+  _id: string;
+  _rev: string;
+  _updatedAt: string;
+}
+
+interface Image {
+  _type: "image";
+  asset: {
+    _ref: string;
+    _type: "reference";
+  };
+}
+
+interface Curriculum {
+  curriculum: string;
+}
+
+export interface PageInfo extends SanityBody {
+  _type: "pageInfo";
+  name: string;
+  role: string;
+  heroTitle: string;
+  intro: string;
+  avatarImage: Image;
+  curriculum: Curriculum;
+  skills: Skill[];
+  socials: Social[];
+}
+
+export interface Skill extends SanityBody {
+  _type: "skill";
+  image: Image;
+  slug: string;
+  title: string;
+}
+
+export interface Tag extends SanityBody {
+  _type: "tag";
+  title: string;
+}
+
+export interface Social extends SanityBody {
+  _type: "social";
+  link: string;
+  title: string;
+}
+
+export interface Project extends SanityBody {
+  _type: "projects";
+  projectLink: string;
+  projectGithubLink: string;
+  projectTitle: string;
+  projectImage: Image;
+  tags: Tag[];
+}
