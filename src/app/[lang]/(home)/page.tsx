2b058a91ce437d25d54aa249474c75a3461a16ec
Tarcisio <tarcisioandrade016@gmail.com>
2023-11-11 15:41:51 -0300

    chore: applied section component in home page

--- src/app/[lang]/(home)/page.tsx
+++ src/app/[lang]/(home)/page.tsx
@@ -1,4 +1,6 @@
 import ButtonLink from "@/components/ButtonLink";
+import ProjectItem from "@/components/ProjectItem";
+import Section from "@/components/Section";
 import SocialButtons from "@/components/SocialButtons";
 import { RouteParams } from "@/interfaces/route";
 import { getDictionary } from "@/lib/getDictionary";
@@ -11,38 +13,57 @@ export default async function Home({ params }: RouteParams) {
   const t = await getDictionary(params.lang);
 
   return (
-    <section className="h-[1000px]">
-      <div className="flex flex-col gap-12">
-        <div className="w-fit overflow-hidden rounded">
-          <Image
-            src={urlForImage(profileData.avatarImage)
-              .format("webp")
-              .width(100)
-              .height(100)
-              .url()}
-            alt={profileData.name}
-            width={100}
-            height={100}
-          />
+    <>
+      <Section className="pt-24">
+        <div className="flex flex-col gap-12">
+          <div className="w-fit overflow-hidden rounded">
+            <Image
+              src={urlForImage(profileData.avatarImage)
+                .format("webp")
+                .width(100)
+                .height(100)
+                .url()}
+              alt={profileData.name}
+              width={100}
+              height={100}
+            />
+          </div>
+          <div className="max-w-[800px]">
+            <h1 className="text-4xl">{profileData.heroTitle}</h1>
+            <p className="mt-4 text-lg leading-relaxed text-zinc-500">
+              {profileData.intro}
+            </p>
+          </div>
+          <div className="flex flex-wrap items-center gap-4">
+            <SocialButtons socials={profileData.socials} />
+            <ButtonLink className="w-64" href={`${profileData.curriculum}?dl=`}>
+              {t.buttonCV}
+            </ButtonLink>
+          </div>
         </div>
-        <div className="max-w-[800px]">
-          <h1 className="text-4xl">{profileData.heroTitle}</h1>
-          <p className="mt-4 text-lg leading-relaxed text-zinc-500">
-            {profileData.intro}
-          </p>
-        </div>
-        <div className="flex flex-wrap items-center gap-4">
-          <SocialButtons socials={profileData.socials} />
-          <ButtonLink className="w-64" href={`${profileData.curriculum}?dl=`}>
-            {t.buttonCV}
-          </ButtonLink>
-        </div>
-      </div>
 
-      <div className="absolute right-4 top-24 flex items-center gap-3 self-start whitespace-nowrap rounded bg-green-700/10 px-4 py-2 text-green-500 sm:right-12 lg:top-12">
-        <div className="h-2 w-2 rounded-full bg-current" />
-        Open to work
-      </div>
-    </section>
+        <div className="absolute right-4 top-24 flex items-center gap-3 self-start whitespace-nowrap rounded bg-green-700/10 px-4 py-2 text-green-500 sm:right-12 lg:top-12">
+          <div className="h-2 w-2 rounded-full bg-current" />
+          Open to work
+        </div>
+      </Section>
+
+      <Section>
+        <h2 className="text-3xl">Projetos</h2>
+
+        <div className="mt-12 flex grid-cols-1 flex-col gap-12 sm:grid sm:grid-cols-2 xl:grid-cols-3">
+          {profileData.projects.map((project) => (
+            <ProjectItem key={project._id} project={project} />
+          ))}
+        </div>
+        <ButtonLink href="/project" className="mt-12 text-white">
+          Ver Todos
+        </ButtonLink>
+      </Section>
+
+      <Section>
+        <p>oi</p>
+      </Section>
+    </>
   );
 }
