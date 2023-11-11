d592e0b5ff6e0cb1759d2e1d49a1b3cab07671d2
Tarcisio <tarcisioandrade016@gmail.com>
2023-11-11 15:44:03 -0300

    feat: button link trigger function

--- src/components/ButtonLink.tsx
+++ src/components/ButtonLink.tsx
@@ -2,20 +2,22 @@ import React, { ComponentProps, ReactNode } from "react";
 import { cn } from "@/lib/utils";
 import Link from "next/link";
 
-type Props = ComponentProps<"a"> & {
+type Props = {
   children: ReactNode;
   href: string;
   className?: string;
+  target?: React.HTMLAttributeAnchorTarget;
 };
 
-const ButtonLink = ({ children, href, className }: Props) => {
+const ButtonLink = ({ children, href, className, ...props }: Props) => {
   return (
     <Link
       href={href}
       className={cn(
-        "flex h-12 items-center justify-center rounded border border-zinc-800 px-4 text-zinc-500 transition-all hover:bg-zinc-800/20 hover:text-white",
+        "flex h-12 w-full items-center justify-center rounded border border-zinc-800 px-4 text-zinc-500 transition-all hover:bg-zinc-800/20 hover:text-white",
         className,
       )}
+      {...props}
     >
       {children}
     </Link>
