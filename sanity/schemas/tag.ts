d4a40d7f0d379b2deb314b34e9d3de68e4b773bd
Tarcisio <tarcisioandrade016@gmail.com>
2023-11-11 15:41:02 -0300

    feat: added a new tagCategory schema reference in tag

--- sanity/schemas/tag.ts
+++ sanity/schemas/tag.ts
@@ -6,6 +6,26 @@ const tag = {
       title: "Title",
       type: "string",
     },
+    {
+      name: "link",
+      title: "Link",
+      type: "url",
+    },
+    {
+      name: "image",
+      options: {
+        hotspot: true,
+      },
+      title: "Image",
+      type: "image",
+    },
+
+    {
+      name: "category",
+      to: [{ type: "tagCategory" }],
+      title: "Category",
+      type: "reference",
+    },
   ],
   name: "tag",
   title: "Tag",
