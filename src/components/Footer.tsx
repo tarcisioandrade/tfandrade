801326a4b26828a2ebc695fca56bbec9c7d0619f
Tarcisio <tarcisioandrade016@gmail.com>
2024-01-03 15:18:18 -0300

    feat: dynamic year in footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -8,7 +8,7 @@ const Footer = () => {
     <footer className="h-24 lg:pl-[250px]">
       <div className="flex h-full items-center justify-center gap-12 px-4 sm:px-12">
         <p className="text-zinc-500">
-          © 2023 {t("developer")}{" "}
+          © {new Date().getFullYear()} {t("developer")}{" "}
           <strong className="text-white">{t("me")}</strong>
         </p>
       </div>
