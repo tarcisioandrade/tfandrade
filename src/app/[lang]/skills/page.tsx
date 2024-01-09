1f03b5ddcb5e62279013b5d27ee954d225ef66f6
Tarcisio <tarcisioandrade016@gmail.com>
2024-01-09 00:03:38 -0300

    feat: logic to filter skills when parameter changes

--- src/app/[lang]/skills/page.tsx
+++ src/app/[lang]/skills/page.tsx
@@ -1,9 +1,13 @@
 import React from "react";
 import Section from "@/components/Section";
 import SkillBadge from "@/components/SkillBadge";
+import SkillsFilter from "@/components/SkillsFilter";
+import { getCategoriesFilters } from "@/services/getCategoriesFilters";
 import { getSkills } from "@/services/getSkills";
 import { Metadata } from "next";
 import { getTranslations, unstable_setRequestLocale } from "next-intl/server";
+
+export const dynamic = "force-dynamic";
 
 export async function generateMetadata({
   params,
@@ -17,17 +21,39 @@ export async function generateMetadata({
   };
 }
 
-const SkillPage = async ({ params }: { params: { lang: string } }) => {
+const SkillPage = async ({
+  params,
+  searchParams,
+}: {
+  params: { lang: string };
+  searchParams?: { category: string | undefined };
+}) => {
   unstable_setRequestLocale(params.lang);
 
   const skills = await getSkills(params.lang);
+  const categoriesFilters = await getCategoriesFilters(params.lang);
   const t = await getTranslations("sectionTitles");
+
+  const skillsFilteredByQueryParamsCategory = skills.filter((skill) => {
+    const isAllCategories =
+      !searchParams?.category || searchParams.category === "all";
+    const isFullstackForBackendOrFrontend =
+      (searchParams?.category === "backend" ||
+        searchParams?.category === "frontend") &&
+      skill.category.name === "fullstack";
+    const isMatchingCategory = skill.category.name === searchParams?.category;
+
+    return (
+      isAllCategories || isFullstackForBackendOrFrontend || isMatchingCategory
+    );
+  });
 
   return (
     <Section className="min-h-[calc(100vh_-100px)]">
       <h1 className="text-4xl">{t("skills")}</h1>
+      <SkillsFilter filtersOptions={categoriesFilters} />
       <div className="mt-12 flex flex-col gap-6 sm:grid sm:grid-cols-2 xl:grid-cols-4">
-        {skills.map((skill) => (
+        {skillsFilteredByQueryParamsCategory.map((skill) => (
           <SkillBadge key={skill._id} skill={skill} />
         ))}
       </div>
