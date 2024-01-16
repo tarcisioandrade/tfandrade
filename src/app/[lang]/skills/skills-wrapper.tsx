dcd9ee5df186a9317c03f8f56ddcc9ed1becff22
Tarcisio <tarcisioandrade016@gmail.com>
2024-01-16 12:31:16 -0300

    feat: skills-wrapper component

---
+++ src/app/[lang]/skills/skills-wrapper.tsx
@@ -0,0 +1,38 @@
+"use client";
+
+import { Suspense } from "react";
+import SkillBadge from "@/components/SkillBadge";
+import { Skill } from "@/interfaces/sanity";
+import { useSearchParams } from "next/navigation";
+
+type Props = {
+  skills: Skill[];
+};
+
+const SkillsWrapper = ({ skills }: Props) => {
+  const category = useSearchParams().get("category");
+
+  const skillsFilteredByQueryParamsCategory = skills.filter((skill) => {
+    const isAllCategories = !category || category === "all";
+    const isFullstackForBackendOrFrontend =
+      (category === "backend" || category === "frontend") &&
+      skill.category.name === "fullstack";
+    const isMatchingCategory = skill.category.name === category;
+
+    return (
+      isAllCategories || isFullstackForBackendOrFrontend || isMatchingCategory
+    );
+  });
+
+  return (
+    <div className="mt-12 flex flex-col gap-6 sm:grid sm:grid-cols-2 xl:grid-cols-4">
+      {skillsFilteredByQueryParamsCategory.map((skill) => (
+        <Suspense key={skill._id} fallback={<p>oi</p>}>
+          <SkillBadge skill={skill} />
+        </Suspense>
+      ))}
+    </div>
+  );
+};
+
+export default SkillsWrapper;
