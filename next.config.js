26b8579351735a1eb0b2495b0c999ee5ef5ca74e
Tarcisio <tarcisioandrade016@gmail.com>
2024-10-11 10:10:56 -0300

    chore: remove admin route in build

--- next.config.js
+++ next.config.js
@@ -1,3 +1,5 @@
+const { rename } = require("node:fs");
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
@@ -8,5 +10,12 @@ const nextConfig = {
     ],
   },
 };
+const SKIP_ADMIN_ROUTE = process.env.SKIP_ADMIN_ROUTE === "true";
 
+if (SKIP_ADMIN_ROUTE) {
+  rename("src/app/admin", "src/app/_admin", (err) => {
+    if (err) throw err;
+    console.log("Pasta renomeada com sucesso!");
+  });
+}
 module.exports = nextConfig;
